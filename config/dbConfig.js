cdad0dcf149609ed37eb3e5aa91a7aff00ca7a4a
mubasir umbi <mubasirumbi@gmail.com>
2025-03-22 14:49:42 +0530

    db config file updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,25 @@
+// const mongoose = require('mongoose');
+
+// const dbConfig = () => {
+//     const DB = process.env.MONGO_URI || 'mongodb://localhost:27017/item-crud';
+//     mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+//         .then(() => console.log('MongoDB connected successfully'))
+//         .catch(err => console.log('MongoDB connection error:', err));
+// };
+
+// module.exports = dbConfig;
+
+
 const mongoose = require('mongoose');
 
-const dbConfig = () => {
-    const DB = process.env.MONGO_URI || 'mongodb://localhost:27017/item-crud';
-    mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('MongoDB connected successfully'))
-        .catch(err => console.log('MongoDB connection error:', err));
+const dbConfig = async () => {
+    try {
+        const DB = process.env.MONGO_URI || 'mongodb://localhost:27017/item-crud';
+        await mongoose.connect(DB);
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
 };
 
 module.exports = dbConfig;
